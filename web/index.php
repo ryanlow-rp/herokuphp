ed82e660e8bc67e3b09ab80ed77c8359072aad46
Ryan Low <ryan_low@rp.edu.sg>
2022-06-20 19:55:17 +0800

    initial commit

--- web/index.php
+++ web/index.php
@@ -1,11 +1,17 @@
+<!-- fix : Page lacks the HTML doctype, thus triggering quirks mode -->
 <!DOCTYPE html>
+<!-- fix : <html> element does not have a [lang] attribute -->
 <html lang="en">
 
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <!-- fix : Does not have a <meta name="viewport"> tag with width or initial-scale -->
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- fix : Document doesn't have a <title> element -->
   <title>Document</title>
+  <!-- fix : Document does not have a meta description -->
+  <meta name="description" content="Put your description here.">
 </head>
 
 <body>
