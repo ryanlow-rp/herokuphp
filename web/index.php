ebebd5f04de69e56612513e5addce7cbf20fe00f
Ryan Low <ryan_low@rp.edu.sg>
2022-06-20 19:40:11 +0800

    initial commit

--- web/index.php
+++ web/index.php
@@ -3,6 +3,8 @@
 
 <head>
   <title>Title</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="Put your description here.">
 </head>
 
 <body>
