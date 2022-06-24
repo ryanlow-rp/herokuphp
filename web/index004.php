26ffadfca5c06b5baa186f04de95f6967de4c2a9
Ryan Low <ryan_low@rp.edu.sg>
2022-06-24 20:58:44 +0800

    commit message goes here

---
+++ web/index004.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <meta name="description" content="Put your description here.">
+  <script src="js/chat.js"></script>
+</head>
+
+<body>
+  <p>
+    Hello World!
+  </p>
+</body>
+
+</html>
