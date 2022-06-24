68c16379b4c1209f3de3193efba2540afc271ad3
Ryan Low <ryan_low@rp.edu.sg>
2022-06-24 20:14:59 +0800

    commit message goes here

--- web/index.php
+++ web/index.php
@@ -12,11 +12,12 @@
   <title>Document</title>
   <!-- fix : Document does not have a meta description -->
   <meta name="description" content="Put your description here.">
-  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <!-- <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="styles.css">
-  <script src="script.js" defer></script>
+  <script src="script.js" defer></script> -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.js"></script>
 </head>
 
 <body>
