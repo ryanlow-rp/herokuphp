d4cc508985f01d2fe975d2dbfcfcb69219a862e6
Ryan Low <ryan_low@rp.edu.sg>
2022-06-26 11:01:15 +0800

    commit message goes here

--- web/test017.php
+++ web/test017.php
@@ -8,7 +8,7 @@
 </head>
 
 <body>
-  <img src="img/wallpaper.jpg" alt="wallpaper image" width="300" height="200">
+  <img src="img/wallpaper.jpg" alt="wallpaper image" width="100%" height="100%">
 </body>
 
 </html>
