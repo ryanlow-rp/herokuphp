4364de9b6f7a2ce626cc837edc95e097c9871b90
Ryan Low <ryan_low@rp.edu.sg>
2022-06-26 16:06:13 +0800

    commit message goes here

--- web/test019.php
+++ web/test019.php
@@ -6,6 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="description" content="Put your description here.">
   <link rel="stylesheet" href="css\style.css">
+  <link rel="stylesheet" href="css\style2.css">
 </head>
 
 <body>
