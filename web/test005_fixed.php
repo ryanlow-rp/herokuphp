26ac1dc0979af7d40475f492ceea3f2bdd15af1e
Ryan Low <ryan_low@rp.edu.sg>
2022-06-26 22:23:41 +0800

    commit message goes here

--- web/test005_fixed.php
+++ web/test005_fixed.php
@@ -1,10 +1,15 @@
+<!-- fix : Page lacks the HTML doctype, thus triggering quirks mode -->
 <!DOCTYPE html>
+<!-- fix : <html> element does not have a [lang] attribute -->
 <html lang="en">
 
 <head>
-  <title>Title</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- fix : Does not have a <meta name="viewport"> tag with width or initial-scale -->
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- fix : Document does not have a meta description -->
   <meta name="description" content="Put your description here.">
+  <!-- fix : Document doesn't have a <title> element -->
+  <title>Document</title>
 </head>
 
 <body>
