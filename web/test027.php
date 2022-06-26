d33ff6586e86fc339debb44a61103f9b66adfa85
Ryan Low <ryan_low@rp.edu.sg>
2022-06-26 18:04:24 +0800

    commit message goes here

--- web/test027.php
+++ web/test027.php
@@ -11,6 +11,11 @@
   <p>
     Hello World!
   </p>
+  <div role="radiogroup">
+    <input role="radio" type="radio" aria-checked="true"> One<br>
+    <input role="radio" type="radio" aria-checked="false"> Two<br>
+    <input role="radio" type="radio" aria-checked="false"> Three
+  </div>
 </body>
 
 </html>
