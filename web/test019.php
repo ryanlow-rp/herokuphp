2dd4a2029090825c5501340e8f2c08092bcc377f
Ryan Low <ryan_low@rp.edu.sg>
2022-06-26 15:52:54 +0800

    commit message goes here

--- web/test019.php
+++ web/test019.php
@@ -5,11 +5,13 @@
   <title>Title</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="description" content="Put your description here.">
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/rxjs/5.0.2/Rx.js"></script>
+  <style src="css\style.css"></style>
 </head>
 
 <body>
-  Hello World!
+  <p>
+    Hello World!
+  </p>
 </body>
 
 </html>
