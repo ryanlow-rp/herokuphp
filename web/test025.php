c1fd09be45677e466527856f203c70b026886bf2
Ryan Low <ryan_low@rp.edu.sg>
2022-06-26 17:37:10 +0800

    commit message goes here

--- web/test025.php
+++ web/test025.php
@@ -5,7 +5,7 @@
   <title>Title</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="description" content="Put your description here.">
-  <script src="https://code.jquery.com/ui/1.13.1/jquery-ui.min.js" integrity="sha256-eTyxS0rkjpLEo16uXTS0uVCS4815lc40K2iVpWDvdSY=" crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/ui/1.13.1/jquery-ui.min.js"></script>
 </head>
 
 <body>
