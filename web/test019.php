ebb188052b15e4177e8dd6361c1152e7a9e83dc4
Ryan Low <ryan_low@rp.edu.sg>
2022-06-26 16:28:58 +0800

    commit message goes here

--- web/test019.php
+++ web/test019.php
@@ -5,6 +5,7 @@
   <title>Title</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="description" content="Put your description here.">
+  <link rel="stylesheet" href="css\missing.css">
 </head>
 
 <body>
--- web/test020.php
+++ web/test020.php
@@ -5,7 +5,13 @@
   <title>Title</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="description" content="Put your description here.">
-  <link rel="stylesheet" href="css\missing.css">
+  <style>
+    p {
+      color: #bb5588;
+      background-color: yellow;
+    }
+  </style>
+
 </head>
 
 <body>
