232817cddd4053073644d531d76d05fbdb045e82
Ryan Low <ryan_low@rp.edu.sg>
2022-06-26 17:04:25 +0800

    commit message goes here

--- web/test022.php
+++ web/test022.php
@@ -6,25 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="description" content="Put your description here.">
   <style>
-    .slick-prev,
-    .slick-next {
-      font-size: 0;
-      line-height: 0;
-      position: absolute;
-      top: 50%;
-      display: block;
-      width: 20px;
-      height: 20px;
-      padding: 0;
-      -webkit-transform: translate(0, -50%);
-      -ms-transform: translate(0, -50%);
-      transform: translate(0, -50%);
-      cursor: pointer;
-      color: transparent;
-      border: none;
-      outline: none;
-      background: transparent;
-    }
+
   </style>
 </head>
 
@@ -32,7 +14,8 @@
   <p>
     Hello World!
   </p>
-  <button type="button" class="slick-prev slick-arrow">Previous</button>
+  <button>Button One</button>
+  <button>Button Two</button>
 </body>
 
 </html>
